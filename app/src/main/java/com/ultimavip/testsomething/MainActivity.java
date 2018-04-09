bd79fc00090b78044ad22823ec5da2043a0ec69c
zhuxing <30117355@qq.com>
2018-04-09 13:58:40 +0800

    项目创建

---
+++ app/src/main/java/com/ultimavip/testsomething/MainActivity.java
@@ -0,0 +1,31 @@
+package com.ultimavip.testsomething;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et1;
+    private EditText et2;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView tv = findViewById(R.id.textView);
+        Button bt = findViewById(R.id.button);
+        et1 = findViewById(R.id.editText);
+        et2 = findViewById(R.id.editText2);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                tv.setText(Integer.parseInt(et1.getText().toString())+ Integer.parseInt(et2.getText().toString()));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/ultimavip/testsomething/utils/MathUtils.java
@@ -0,0 +1,15 @@
+package com.ultimavip.testsomething.utils;
+
+/**
+ * Created by zhuxing on 2018/4/9.
+ */
+
+public class MathUtils {
+    public static int sum(int a1,int a2){
+       int  result=a1+a2;
+        result++;
+        ++result;
+        result++;
+        return result;
+    }
+}
