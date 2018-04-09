e270dd69ff67e2a5e3c11cdf085ea71153b06cae
zhuxing <30117355@qq.com>
2018-04-09 14:21:36 +0800

    提交

--- app/src/main/java/com/ultimavip/testsomething/utils/MathUtils.java
+++ app/src/main/java/com/ultimavip/testsomething/utils/MathUtils.java
@@ -1,5 +1,7 @@
 package com.ultimavip.testsomething.utils;
 
+import android.util.Log;
+
 /**
  * Created by zhuxing on 2018/4/9.
  */
@@ -8,8 +10,12 @@ public class MathUtils {
     public static int sum(int a1,int a2){
        int  result=a1+a2;
         result++;
+        result++;
+        result++;
+        Log.e("MathUtils",""+result);
         ++result;
         result++;
+        ++result;
         return result;
     }
 }
